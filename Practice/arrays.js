4071cecda0f6fa0014b4bb195e1e8b0191ea46f2
Sterling Chin <sterling.chin@gmail.com>
2017-02-03 16:39:19 -0700

    updated folders

--- Practice/arrays.js
+++
@@ -1,5 +0,0 @@
-// Create a function called exponent that takes in two parameters, the first parameter should represent a number to be multiplied against itself and the second parameter should represent how many times it is multiplied by itself. The function should return the result of this operation. exponent(2,3) should return 8
-
-function expoment(x,y){
-  
-}
